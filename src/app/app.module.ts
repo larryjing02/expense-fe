d27cf0eba06b571d65c411bd76998fafc208da49
Larry Jing <larryjing02@gmail.com>
2023-06-24 00:08:38 -0700

    Added components, styled dashboard and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavbarComponent,
+    DashboardComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
