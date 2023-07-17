35e9fac38b61f8827ceebd64d65e92c73a1a430d
Larry Jing <larryjing02@gmail.com>
2023-07-17 14:46:20 -0700

    Resolve chart display timezone bug

--- src/app/services/expense.service.ts
+++ src/app/services/expense.service.ts
@@ -22,8 +22,20 @@ export class ExpenseService {
   }
 
   getExpenseChart(timeRange: string, startDate: string, endDate: string): Observable<any> {
+    const utcStartDate = new Date(startDate);
+    utcStartDate.setHours(0, 0, 0);
+  
+    const utcEndDate = new Date(endDate);
+    utcEndDate.setHours(23, 59, 59);
+    console.log(utcStartDate.toISOString());
+    console.log(utcEndDate.toISOString());
+  
     return this.http.get(API_ENDPOINTS.CHART, {
-      params: { timeRange, startDate, endDate },
+      params: {
+        timeRange,
+        startDate: utcStartDate.toISOString(),
+        endDate: utcEndDate.toISOString()
+      },
       headers: this.headers
     });
   }
